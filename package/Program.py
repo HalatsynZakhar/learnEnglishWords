eb3440927f8d04c481b8225330497ddc55c352a2
halatsynzakhar <halatsynzakhar@gmail.coom>
2023-12-09 22:21:55 +0200

    commit

--- package/Program.py
+++ package/Program.py
@@ -23,48 +23,65 @@ class Program:
         print("Введите перевод слова на английском языке.")
         print("Для выхода из программы введите '123'.")
         print(
-            "Выберите режим тренировки. 1 - все слова кроме изученных, 2 - уже взятые на изучение, 3 - повторение изученных слов")
+            "Выберите режим тренировки. 1 - все слова кроме изученных, 2 - уже взятые на изучение, 3 - повторение изученных слов, 4 - собственный набор слов")
         inp = input()
-
         print("Введите количество слов, которые будут по-кругу изучаться (если не натуральное число, то ограничений нет")
         num =input()
         temp = []
-        try:
+        if num.isdigit():
             num = int(num)
-            if num > 0:
-                while len(temp)!=num+1:
-                    word = self.training.generate_word()
-                    if inp == "1":
-                        if word.getStatus() == "изучено":
-                            continue
-                    elif inp == "2":
-                        if word.getStatus() == "не изучено" or word.getStatus() == "изучено":
-                            continue
-                    elif inp == "3":
-                        if word.getStatus() == "не изучено" or word.getStatus() == "на изучении":
-                            continue
-                    else:
-                        break
+            if num<=0:
+                num = 0
+        else:
+            num = 0
+
+        if inp=="1":
+            while len(temp)!= num:
+                word = self.training.generate_word()
+                if word.getStatus() == "изучено":
+                    continue
+                temp.append(word)
+        if inp=="2":
+            while len(temp)!= num:
+                word = self.training.generate_word()
+                if word.getStatus() == "не изучено" or word.getStatus() == "изучено":
+                    continue
+                temp.append(word)
+        if inp == "3":
+            while len(temp) != num:
+                word = self.training.generate_word()
+                if word.getStatus() == "не изучено" or word.getStatus() == "на изучении":
+                    continue
+                temp.append(word)
+        if inp=="4":
+            while len(temp)!= num:
+                text = input("Введите слово на англ: ")
+
+                word = self.training.find_word(text)
+                if isinstance(word, Word):
                     temp.append(word)
-        except:
-            pass
+                    print("Слово добавлено")
+                else:
+                    print("Слово не было найдено")
+
+
         while True:
             if len(temp)>0:
                 word = random.choice(temp)
             else:
                 word = self.training.generate_word()
 
-            if inp == "1":
-                if word.getStatus() == "изучено":
-                    continue
-            elif inp == "2":
-                if word.getStatus() == "не изучено" or word.getStatus() == "изучено":
-                    continue
-            elif inp == "3":
-                if word.getStatus() == "не изучено" or word.getStatus() == "на изучении":
-                    continue
-            else:
-                break
+                if inp == "1":
+                    if word.getStatus() == "изучено":
+                        continue
+                elif inp == "2":
+                    if word.getStatus() == "не изучено" or word.getStatus() == "изучено":
+                        continue
+                elif inp == "3":
+                    if word.getStatus() == "не изучено" or word.getStatus() == "на изучении":
+                        continue
+                else:
+                    break
 
             print("Текущий уровень: {} из  {}, текущий шаг на уровне: {} из {}.".format(word.getCurrentLvl(),
                                                                                         word.getAllLevel(),
--- package/Training.py
+++ package/Training.py
@@ -8,7 +8,10 @@ class Training:
     def generate_word(self):
         return random.choice(self.words)
 
-
+    def find_word(self, word):
+        for i in self.words:
+            if i.english==word:
+                return i
     def check_answer(self, word, answer):
         if word.getEnglish() == answer:
             word.addStep()
